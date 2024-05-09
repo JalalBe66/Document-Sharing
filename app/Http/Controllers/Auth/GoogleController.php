88a8efcb91a184d619a911bb0c21fce265a8429b
JaBeXIX <jalalbelych011@gmail.com>
2024-05-09 19:37:27 +0100

    files less

---
+++ app/Http/Controllers/Auth/GoogleController.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use Laravel\Socialite\Facades\Socialite;
+use App\Models\Employe;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+
+class GoogleController extends Controller
+{
+    
+    public function googlePage()
+    {
+        return Socialite::driver('google')->redirect();
+    }
+
+    
+    public function googleCallback()
+    {
+        
+            $user = Socialite::driver('google')->user();
+
+        // Vérifie si l'utilisateur existe déjà dans la base de données
+        $existingUser = Employe::where('email', $user->email)->first();
+
+        if ($existingUser) {
+            // Connecte l'utilisateur existant
+            Auth::login($existingUser);
+        } else {
+            // Crée un nouvel utilisateur s'il n'existe pas encore dans la base de données
+            $newUser = new Employe();
+            $newUser->nom = $user->user["family_name"];
+            $newUser->prenom = $user->user['given_name'];
+            $newUser->email = $user->email;
+            $newUser->password = Hash::make($user->password);
+            $newUser->urlProfile = $user->avatar;
+            // Ajoute d'autres champs nécessaires
+            $newUser->save();
+
+            // Connecte le nouvel utilisateur
+            Auth::login($newUser);
+        }
+
+        // Redirige l'utilisateur après la connexion
+        return redirect('/Accueil');
+    }
+}
---
+++ resources/views/employer/profileE.blade.php
@@ -0,0 +1,62 @@
+@extends("templates.templateF")
+@section("css-integration","css/profile.css")
+@section("contenu")
+
+    <div id="GContainnerP">
+        <div id="TContainnerP">
+            
+                <p>Mon Compte </p>
+            
+        </div>
+
+        <form method="post" enctype="multipart/form-data" action="{{route('Profile.edit',auth()->user()->id)}}" id="modiP">
+            @csrf
+            @method("PUT")
+            <div class="titreContent">
+                Modifier vos information 
+            </div>
+            <div class="profileM">
+                <div id="inpM"> 
+                    <div id="contContInp">
+                        <div class="contInp">
+                            <div><label >Nom</label></div>
+                            <div class="inp"><input id="inpV"  placeholder="Nouveau Nom" type="text" name="nom" id=""></div>
+                        </div>
+                        <div class="contInp">
+                            <div><label >Prenom</label></div>
+                            <div class="inp"><input id="inpV" placeholder="Nouveau Prenom" type="text" name="prenom" id=""></div>
+                        </div>
+                    </div>
+                </div>
+                <div id="imgM">
+                    <div id="P-actuel" style="background-image: url('{{auth()->user()->urlProfile}}');background-size:cover;background-position:center;background-repeat:no-repeat"></div>
+                    <div id="P-modify">
+                        <label for="file-upload" class="custom-file-upload">
+                            Choisie une image
+                        </label>
+                        <input name="image" id="file-upload" type="file" />
+                    </div>
+                </div>
+            </div>
+            <div   class="buttonP">
+                    <button id="buttonS-t"  type="submit">Modifier</button>
+            </div>
+            </form>
+            <div id="suppP">
+                <div class="titreContent">
+                    Supprimer votre compte
+                </div>
+                <div id="sup">
+                    <div id="mailS">
+                        <input type="text" disabled value="{{auth()->user()->email}}" placeholder="Saisie votre email" name="email" id="mail">
+                    </div>
+                    <form method="post" action="{{route('Profile.delete',auth()->user()->id)}}" id="inpS">
+                        @csrf
+                        @method("DELETE")
+                        <div class="buttonP"><button id="buttonS-b" type="submit" >Supprimer</button></div>
+                    </form>
+                </div>
+            </div>
+        </div>
+
+@endsection
